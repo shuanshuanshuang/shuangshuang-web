de76f9237aab483d8b9666ef81309e813ea4157e
shuanshuanshuang <61463803+shuanshuanshuang@users.noreply.github.com>
2020-02-29 14:55:09 +0800

    dubbo的web部分调通

--- src/main/java/com/atnanjing/demo/ShuangshuangWebApplication.java
+++ src/main/java/com/atnanjing/demo/ShuangshuangWebApplication.java
@@ -1,11 +1,14 @@
 package com.atnanjing.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @ComponentScan
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ShuangshuangWebApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/atnanjing/demo/controller/IndexController.java
+++ src/main/java/com/atnanjing/demo/controller/IndexController.java
@@ -1,8 +1,27 @@
 package com.atnanjing.demo.controller;
 
+import com.atguigu.demo.dto.UserDTO;
+import com.atguigu.demo.service.UserService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Reference(version = "${demo.service.version}")
+    private UserService userService;
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET)
+    public UserDTO getUser(@PathVariable("id") Long id){
+        UserDTO userById = userService.findUserById(id);
+        return userById;
+    }
     //
 }
